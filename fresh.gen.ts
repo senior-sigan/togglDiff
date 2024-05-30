afff0bc2b7983303bcbc2bbc91ca25135e2ac91a
Ilya Siganov <ilya.blan4@gmail.com>
2024-05-30 11:06:56 +0600

    feat: add registration page without handling form-data

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,6 +4,7 @@
 
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
+import * as $app from "./routes/app.tsx";
 import * as $index from "./routes/index.tsx";
 
 import { type Manifest } from "$fresh/server.ts";
@@ -12,6 +13,7 @@ const manifest = {
   routes: {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
+    "./routes/app.tsx": $app,
     "./routes/index.tsx": $index,
   },
   islands: {},
